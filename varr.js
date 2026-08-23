bc0af42f6358a1d8f1758f2a69280ba0c1b28d23
Ayaz Gouri <ayazgouri.tech@gmail.com>
2026-08-23 11:27:54 +0530

    Add files via upload

---
+++ varr.js
@@ -0,0 +1,38 @@
+// {
+//     let a = 5;
+//     console.log(a);
+// }
+// {
+//     let a = 10;
+//     console.log(a); // block jha se banta h vhi se new block start hota h
+// }
+
+
+
+// err aayegaconst a ;
+// console.log(a);
+
+
+//  num , string boolean undefined , null => data types  hote h 7 data type
+ //num 
+ let age = 24;
+ let price = 200.9;
+
+//string data type
+let fullName = "Shivani jatav";
+
+//boolean
+isFollwe = true;
+
+// undefined
+let x;
+
+//null
+//let X = null;
+
+//brigint
+let X = BrigInt("123");
+
+//symbol
+let y = Symbol("Hello");
+
