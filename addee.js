b6480149989f9311db17c34b28b077092f14f3b4
Ayaz Gouri <ayazgouri.tech@gmail.com>
2026-09-21 12:37:15 +0530

    Add files via upload

---
+++ addee.js
@@ -0,0 +1,17 @@
+console.log("Welcome");
+console.log("shivani");
+console.log("I  Love Java");
+fullName = "shivani jatav";
+age = 21;
+price = 23.45;
+x = null;
+y = undefined;
+// isFollow = true
+fullName = 23;  
+ 
+// console.log(fullName);
+// console.log(age);
+// console.log(price);
+// console.log(x);
+//console.log(isFollow);
+console.log(fullName);
